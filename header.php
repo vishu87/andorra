ff92a0f5387b1040de5a0db0ee276825285089a8
vishu87 <vishu.iitd@gmail.com>
2017-03-01 12:52:15 +0530

    time change hidden

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 	<!-- <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css?v=1.0.1">
 
 	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
 	<script type="text/javascript" src="assets/jquery-ui/jquery-ui.min.js"></script>
